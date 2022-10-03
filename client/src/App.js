2bde179c9535be7c3e8523656185d7a9ceb50f57
Amarjeet2753 <amarjeet275303@gmail.com>
2022-10-03 15:19:12 +0530

    home component created

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import './component/Home'
+import Home from './component/Home';
 
 function App() {
   return (
@@ -15,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <Home/>
         </a>
       </header>
     </div>
